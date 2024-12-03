e9fa073ce414b667ab26201692ae9b1576c999fc
MyaM-F <fullermy@shernet.sheridancollege.ca>
2024-12-03 17:20:55 -0500

    Turns and Score added

---
+++ src/ca/sheridancollege/project/Score.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author myame
+ */
+public class Score {
+    private int points;
+
+    public Score() {
+        this.points = 0;
+    }
+
+    public void addPoint() {
+        this.points++;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+}
