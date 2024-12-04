374f5dbc486bdb526602d5df62cbdd16558373d7
ellioats <emwcurrie@gmail.com>
2024-12-04 16:51:47 -0500

    added GoFishCard.java

---
+++ src/ca/sheridancollege/project/GoFishCard.java
@@ -0,0 +1,44 @@
+
+import ca.sheridancollege.project.Card;
+
+public class GoFishCard extends Card {
+
+    private boolean isRequested; 
+    private String suit;
+    private String rank;
+    
+    // no empty constructor because card must have a suit and rank
+    public GoFishCard(String suit, String rank) {
+        // add validation?
+        this.suit = suit;
+        this.rank = rank;
+    }
+    
+    
+    @Override
+    public String toString() {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return "";
+    }
+    
+    public void markAsRequetsed() {
+        this.isRequested = true;
+    }
+    
+    public void resetRequestedStatus() {
+        this.isRequested = false;
+    }
+    
+    public boolean isRequested() {
+        return this.isRequested;
+    }
+    
+    public String getSuit() {
+        return this.suit;
+    }
+    
+    public String getRank() {
+        return this.rank;
+    }
+    
+}
