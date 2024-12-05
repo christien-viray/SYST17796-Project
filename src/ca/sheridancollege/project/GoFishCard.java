15ea34b4cccd85ee33871a46046caad57c611b3a
ellioats <emwcurrie@gmail.com>
2024-12-05 13:45:32 -0500

    began impl. askForCard()

--- src/ca/sheridancollege/project/GoFishCard.java
+++ src/ca/sheridancollege/project/GoFishCard.java
@@ -1,44 +1,46 @@
+package ca.sheridancollege.project;
 
 import ca.sheridancollege.project.Card;
 
 public class GoFishCard extends Card {
 
-    private boolean isRequested; 
+    private boolean isRequested;
     private String suit;
     private String rank;
-    
+
     // no empty constructor because card must have a suit and rank
     public GoFishCard(String suit, String rank) {
         // add validation?
         this.suit = suit;
         this.rank = rank;
     }
-    
-    
+
     @Override
     public String toString() {
-//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        // throw new UnsupportedOperationException("Not supported yet."); // Generated
+        // from
+        // nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
         return "";
     }
-    
+
     public void markAsRequetsed() {
         this.isRequested = true;
     }
-    
+
     public void resetRequestedStatus() {
         this.isRequested = false;
     }
-    
+
     public boolean isRequested() {
         return this.isRequested;
     }
-    
+
     public String getSuit() {
         return this.suit;
     }
-    
+
     public String getRank() {
         return this.rank;
     }
-    
+
 }
--- src/ca/sheridancollege/project/GoFishPlayer.java
+++ src/ca/sheridancollege/project/GoFishPlayer.java
@@ -11,17 +11,28 @@ import ca.sheridancollege.project.GoFishDeck;
  *
  */
 
-public class GoFishPlayer {
+public class GoFishPlayer extends Player {
 
     private Hand hand;
-    private String name;
 
     public GoFishPlayer(String name) {
-        this.name = name;
+        super(name);
     }
 
-    public boolean askForCard(Player otherPlayer, String rank) {
+    public boolean askForCard(GoFishPlayer otherPlayer, String rank, String suit) {
         // TODO: implement
+
+        // temp card for comparison
+
+        GoFishCard tempcard = new GoFishCard(suit, rank);
+
+        // other player has requested card
+        if (otherPlayer.getHand().getCards().contains(tempcard)) {
+            // System.out.println(otherPlayer.getName() + " Has the " + rank + " of " +
+            // suit);
+
+        }
+
     }
 
     public void goFish(GoFishDeck deck) {
@@ -34,6 +45,12 @@ public class GoFishPlayer {
 
     public Hand getHand() {
         return this.hand;
+    }
+
+    @Override
+    public void play() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'play'");
     }
 
 }
