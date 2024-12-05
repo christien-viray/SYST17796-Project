2b6d737707cc73bdb93f60e6639e76d1a8a6bd8d
manar-altumah22 <altumahm@sheridancollege.ca>
2024-12-05 14:59:00 -0500

    Update Turns.java PlayerManager Update it

--- src/ca/sheridancollege/project/Turns.java
+++ src/ca/sheridancollege/project/Turns.java
@@ -11,15 +11,21 @@ import java.util.List;
  * @author myame
  */
 public class Turns {
+    private Game game;  // Composition relationship with Game
+    private int currentPlayerIndex;
     private Player currentPlayer; 
     private List<Player> players; 
 
     public Turns(List<Player> players) {
+        this.game = game;
+        this.currentPlayerIndex = 0; // The first player starts the game
         this.players = players;
         this.currentPlayer = players.get(0); 
+
     }
 
     public Player getCurrentPlayer() {
+        return game.getPlayerManager().getPlayers().get(currentPlayerIndex);
         return currentPlayer;
     }
 
@@ -27,5 +33,10 @@ public class Turns {
         int currentIndex = players.indexOf(currentPlayer);
         int nextIndex = (currentIndex + 1) % players.size();
         this.currentPlayer = players.get(nextIndex);
+        int playerCount = game.getPlayerManager().getPlayers().size();
+        if (playerCount > 0) {
+            currentPlayerIndex = (currentPlayerIndex + 1) % playerCount;
+            // Logic to notify the next player that it's their turn could be added here.
+
     }
 }
