f902ac1b52bda692e61c08c6a1e6002a3a94911b
ellioats <emwcurrie@gmail.com>
2024-12-05 09:40:33 -0500

    implemented isStillValid()

--- src/ca/sheridancollege/project/GoFishGame.java
+++ src/ca/sheridancollege/project/GoFishGame.java
@@ -20,6 +20,28 @@ public class GoFishGame extends Game {
     
     // TODO: Still to be implemented
     public boolean isValidInput(String input) {
+        boolean returnVal = false;
+        
+        String[] suits = {"spades", "clubs", "hearts", "diamonds"};
+        String[] nums = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        String[] honor = {"jack", "queen", "king", "ace"};
+        
+        for (String a : suits) {
+            if (input.contains(a))
+                returnVal = true;
+        }
+        
+        for (String a : nums) {
+            if (input.contains(a))
+                returnVal = true;
+        }
+        
+        for (String a : honor) {
+            if (input.contains(a))
+                returnVal = true;
+        }
+        
+        return returnVal;
         
     }
     
