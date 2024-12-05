123f7751bed7a74c1b14684a8237d6e1c01d9cb9
manar-altumah22 <altumahm@sheridancollege.ca>
2024-12-05 14:52:01 -0500

    Updates PlayerManager.java

--- src/ca/sheridancollege/project/PlayerManager.java
+++ src/ca/sheridancollege/project/PlayerManager.java
@@ -1,5 +1,28 @@
 package ca.sheridancollege.project;
 
-public class PlayerManager {
+import java.util.ArrayList;
+import java.util.List;
 
+public class PlayerManager {
+    private List<Player> players;
+    private Game game;  // Composition relationship with Game, Game must exist for PlayerManager to exist.
+
+    public PlayerManager(Game game) {
+        this.game = game;
+        this.players = new ArrayList<>();
+    }
+
+    // Method to add a player to the game
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    // Method to remove a player from the game
+    public void removePlayer(Player player) {
+        players.remove(player);
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
 }
